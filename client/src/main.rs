aac6d1df02de51448bf6e77e6e4583f171ce400a
Sam <tritoke@protonmail.com>
2023-03-20 02:08:44 +0000

    ✨ Sent and deserialized a message for the first time.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,86 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use aucpace::Client;
+use clap::Parser;
+use serialport::SerialPortType;
+use std::io::{ErrorKind, Read, Write};
+use std::time::Duration;
+
+const USART_BAUD: u32 = 115200;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the USB port to open
+    #[arg(short, long)]
+    port: Option<String>,
+
+    /// List USB ports on the system
+    #[arg(long)]
+    list_ports: bool,
+
+    /// Perform partial augmentation
+    #[arg(long = "partial")]
+    partial_aug: bool,
+
+    /// Perform strong AuCPace
+    #[arg(long)]
+    strong: bool,
+
+    /// Perform implicit mutual authentication instead of explicit mutual authentication
+    #[arg(long)]
+    implicit: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::try_parse()?;
+
+    // list the ports if the user asks for it
+    if args.list_ports {
+        let mut ports = serialport::available_ports()?;
+        ports.retain(|port| matches!(port.port_type, SerialPortType::UsbPort(_)));
+        eprintln!("Found the following USB ports:");
+        for port in ports {
+            eprintln!("{}", port.port_name);
+        }
+
+        return Ok(());
+    }
+
+    // open the serial port connection
+    let port_name = args
+        .port
+        .ok_or_else(|| anyhow!("Must supply a USB port."))?;
+    let mut serial = serialport::new(port_name, USART_BAUD).open()?;
+    serial.set_timeout(Duration::from_secs(1))?;
+
+    // start the client
+    let mut base_client = Client::new(rand_core::OsRng);
+    let mut buf = [0u8; 1024];
+    let (client, msg) = base_client.begin();
+    let ser_msg = postcard::to_slice_cobs(&msg, &mut buf)?;
+    serial.write_all(ser_msg)?;
+
+    // send and receive messages :)
+    let mut buf = [0u8; 1024];
+    loop {
+        match serial.read(&mut buf) {
+            Ok(count) => {
+                eprintln!(
+                    "Received - {:02X?} :: {:?}",
+                    &buf[..count],
+                    String::from_utf8_lossy(&buf[..count])
+                );
+            }
+            Err(e) => {
+                if e.kind() != ErrorKind::TimedOut {
+                    eprintln!("Encountered unrecognised error - e={e:?}");
+                }
+            }
+        };
+
+        serial.write(b"Beans")?;
+        eprintln!("Sent - \"Beans\"");
+    }
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,169 +1,96 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
-use bsp::board;
-use teensy4_bsp as bsp;
-use teensy4_panic as _;
-use bsp::hal::timer::Blocking;
-use usb_device::prelude::*;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+use aucpace::{AuCPaceServer, ClientMessage};
+use core::any::Any;
 use core::fmt::Write as _;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::interrupt;
+use embassy_stm32::usart::{Config, Uart};
+use embassy_time::Instant;
+use heapless::String;
+use rand_chacha::ChaChaRng;
+use rand_core::{RngCore, SeedableRng};
+use {defmt_rtt as _, panic_probe as _};
 
-/// CHANGE ME to vary the baud rate.
-const UART_BAUD: u32 = 115200;
-/// Milliseconds to delay before toggling the LED and writing text outputs.
-const DELAY_MS: u32 = 5;
+const K1: usize = 16;
 
-/// Static allocations for use with USB serial
-static EP_MEMORY: EndpointMemory<1024> = EndpointMemory::new();
-static EP_STATE: EndpointState = EndpointState::max_endpoints();
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let p = embassy_stm32::init(Default::default());
+    info!("Initialised peripherals.");
 
-#[bsp::rt::entry]
-fn main() -> ! {
-    // These are peripheral instances. Let the board configure these for us.
-    // This function can only be called once!
-    let instances = board::instances();
+    // configure USART2 which goes over the USB port on this board
+    let config = Config::default();
+    let irq = interrupt::take!(USART2);
+    let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, irq, p.DMA1_CH6, p.DMA1_CH5, config);
+    info!("Configured USART2.");
 
-    // Driver resources that are configured by the board. For more information,
-    // see the `board` documentation.
-    let board::Resources {
-        // `pins` has objects that represent the physical pins. The object
-        // for pin 13 is `p13`.
-        pins,
-        // This is a hardware timer. We'll use it for blocking delays.
-        mut gpt1,
-        // These are low-level USB resources. We'll pass these to a function
-        // that sets up USB logging.
-        usb,
-        // This is the GPIO2 port. We need this to configure the LED as a
-        // GPIO output.
-        mut gpio2,
-        lpuart2,
-        ..
-    } = board::t41(instances);
+    // configure the RNG, kind of insecure but this is just a demo and I don't have real entropy
+    let now = Instant::now().as_micros();
+    let mut rng = ChaChaRng::seed_from_u64(now);
+    info!("Seeded RNG - seed = {}", now);
 
-    // This configures the LED as a GPIO output.
-    let led = board::led(&mut gpio2, pins.p13);
+    // create our AuCPace server
+    let _server: AuCPaceServer<sha2::Sha512, _, K1> = AuCPaceServer::new(rng);
+    // let mut curr_state: Option<dyn Any> = None;
 
-    // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
-    // constants below for more information.
-    gpt1.disable();
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
-
-    // Configures the USB as an CDC-ACM type device - for performing serial communications
-    let bus_adapter = BusAdapter::new(
-        usb,
-        &EP_MEMORY,
-        &EP_STATE,
-    );
-    bus_adapter.gpt_mut(Instance::Gpt0, |gpt| {
-        gpt.stop(); // Stop the timer, just in case it's already running...
-        gpt.clear_elapsed(); // Clear any outstanding elapsed flags
-        gpt.set_interrupt_enabled(false);
-        gpt.set_load(4000);
-        gpt.set_mode(Mode::Repeat); // Repeat the timer after it elapses
-        gpt.reset(); // Load the value into the counter
-        gpt.run(); // start the timer running
-    });
-    bus_adapter.configure();
-
-    // Not sure which endpoints the CDC ACM class will pick,
-    // so enable the setting for all non-zero endpoints.
-    for idx in 1..8 {
-        for dir in &[usb_device::UsbDirection::In, usb_device::UsbDirection::Out] {
-            let ep_addr = usb_device::endpoint::EndpointAddress::from_parts(idx, *dir);
-            // CDC class requires that we send the ZLP.
-            // Let the hardware do that for us.
-            bus_adapter.enable_zlt(ep_addr);
-        }
-    }
-
-    let bus_allocator = UsbBusAllocator::new(bus_adapter);
-    let mut serial = SerialPort::new(&bus_allocator);
-    // Arduino Serial 16c0:0483
-    // imxrt-log 5824:27dd
-    let mut usb_dev = UsbDeviceBuilder::new(&bus_allocator, UsbVidPid(0x5824, 0x27dd))
-        .manufacturer("ACME Ltd")
-        .product("Serial Port")
-        .serial_number("69420")
-        .device_class(USB_CLASS_CDC)
-        .build();
-
-    // Create the UART driver using pins 14 and 15.
-    // Cast it to a embedded_hal trait object so we can
-    // use it with the write! macro.
-    let mut lpuart2: board::Lpuart2 = board::lpuart(lpuart2, pins.p14, pins.p15, UART_BAUD);
-    let lpuart2: &mut dyn embedded_hal::serial::Write<u8, Error = _> = &mut lpuart2;
-
-    // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
-    let mut counter = 0u32;
-
+    // allocate a buffer for communicating with the host
+    let mut s: String<1024> = String::new();
+    let mut buf = [0u8; 1024];
+    let mut idx = 0;
     loop {
-        if counter % (1000 / DELAY_MS) == 0 {
-            led.toggle();
+        // read as much as we can off the wire
+        let count = unwrap!(usart.read_until_idle(&mut buf[idx..]).await);
+        let zero_idx = if count == 0 {
+            continue;
+        } else {
+            // calculate the index of zero in the buffer
+            let zero_idx = buf[idx..idx + count]
+                .iter()
+                .position(|x| *x == 0)
+                .map(|pos| pos + idx);
+
+            // log that we managed to read some data
+            info!(
+                "Read {} bytes - {:02X} - {:?}",
+                count,
+                buf[idx..idx + count],
+                zero_idx
+            );
+
+            // update state
+            idx += count;
+
+            zero_idx
+        };
+
+        if let Some(zi) = zero_idx {
+            // attempt to parse as a message
+            core::write!(
+                s,
+                "Parsed message: {:?}",
+                postcard::from_bytes_cobs::<ClientMessage<K1>>(&mut buf[..=zi]).ok()
+            )
+            .ok();
+            info!("{}", s.as_str());
+
+            // reset the state
+            // copy all the data we read after the 0 byte to the start of the buffer
+            buf.copy_within(zi + 1..idx, 0);
+            idx = idx.saturating_sub(zi + 1);
         }
 
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-
-            match serial.read(&mut buf[..]) {
-                Ok(count) => {
-                    // count bytes were read to &buf[..count]
-                    write!(lpuart2, "Read {:?}\r\n", &buf[..count]).ok();
-                },
-
-                // No data received
-                Err(UsbError::WouldBlock) => {
-                    write!(lpuart2, "Read would block\r\n").ok();
-                }
-
-                // An error occurred
-                Err(err) => {
-                    write!(lpuart2, "Read error - err={err:?}\r\n").ok();
-                }
-            };
-
-            match serial.write(&[0x3a, 0x29]) {
-                Ok(count) => {
-                    // count bytes were written
-                    write!(lpuart2, "Wrote {count} bytes\r\n").ok();
-                },
-
-                // No data could be written (buffers full)
-                Err(UsbError::WouldBlock) => {
-                    write!(lpuart2, "Write failed - would block\r\n").ok();
-                },
-
-                // An error occurred
-                Err(err) => {
-                    write!(lpuart2, "Write error - err={err:?}\r\n").ok();
-                }
-            };
+        if idx == buf.len() {
+            // we've reached the end of the buffer and haven't found a message
+            // so reset the state by just clearing the buffer
+            idx = 0;
+            warn!("Weird state encountered - filled entire buffer without finding message.");
         }
 
-        if counter % (1000 / DELAY_MS) == 0 {
-            write!(lpuart2, "Nice cock - counter = {counter}\r\n").ok();
-        }
-        delay.block_ms(DELAY_MS);
-        counter = counter.wrapping_add(1);
+        // send the message back
+        // unwrap!(usart.write(&buf[..count]).await);
     }
 }
-
-// We're responsible for configuring our timers.
-// This example uses PERCLK_CLK as the GPT1 clock source,
-// and it configures a 1 KHz GPT1 frequency by computing a
-// GPT1 divider.
-use bsp::hal::gpt::ClockSource;
-use teensy4_bsp::hal::usbd::{BusAdapter, EndpointMemory, EndpointState};
-use teensy4_bsp::hal::usbd::gpt::{Instance, Mode};
-use usb_device::bus::UsbBusAllocator;
-
-/// The intended GPT1 frequency (Hz).
-const GPT1_FREQUENCY: u32 = 1_000;
-/// Given this clock source...
-const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-/// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
-/// we need a divider of...
-const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
